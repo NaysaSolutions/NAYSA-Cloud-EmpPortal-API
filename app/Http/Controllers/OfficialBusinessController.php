fa874224ffd145c22e268ead8423f7d22fba0fe7
NAYSA-Solutions Inc. <technical.naysa@gmail.com>
2025-01-03 18:44:43 +0800

    Leave OB and OT Controller

---
+++ app/Http/Controllers/OfficialBusinessController.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+
+class OfficialBusinessController extends Controller
+{
+    
+
+// ** OB Approval Inquiry Current
+public function getApprInq(Request $request) {
+
+    $request->validate([
+        'EMP_NO' => 'required|string',
+    ]);
+
+    $stat = $request->input('STAT');
+    $employee_no = $request->input('EMP_NO');
+
+
+    try {
+        $results = DB::select(
+            'EXEC sproc_PHP_EmpInq_OfficialBusiness @mode = ?, @stat = ?, @emp =?',
+            ['ApprInq', $stat, $employee_no] 
+        );
+
+        return response()->json([
+            'success' => true,
+            'data' => $results,
+        ], 200);
+    } catch (\Exception $e) {
+        return response()->json([
+            'success' => false,
+            'message' => $e->getMessage(),
+        ], 500);
+    }
+
+
+}
+
+
+
+
+
+ // ** OB Approval Inquiry HIstory
+public function getApprHistory(Request $request) {
+
+    $request->validate([
+        'EMP_NO' => 'required|string',
+        'START_DATE' => 'required|date_format:Y-m-d',
+        'END_DATE' => 'required|date_format:Y-m-d',
+    ]);
+
+    $employee_no = $request->input('EMP_NO');
+    $start_date = $request->input('START_DATE');
+    $end_date = $request->input('END_DATE');
+
+    try {
+        $results = DB::select(
+            'EXEC sproc_PHP_EmpInq_OfficialBusiness @mode = ?, @startdate = ?, @enddate =?, @emp =?',
+            ['ApprHistory', $start_date, $end_date, $employee_no] 
+        );
+
+        return response()->json([
+            'success' => true,
+            'data' => $results,
+        ], 200);
+    } catch (\Exception $e) {
+        return response()->json([
+            'success' => false,
+            'message' => $e->getMessage(),
+        ], 500);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// ** OB Application Inquiry Current
+public function getAppInq(Request $request) {
+
+    $request->validate([
+        'EMP_NO' => 'required|string',
+    ]);
+
+    $stat = $request->input('STAT');
+    $employee_no = $request->input('EMP_NO');
+
+
+    try {
+        $results = DB::select(
+            'EXEC sproc_PHP_EmpInq_OfficialBusiness @mode = ?, @emp =?',
+            ['AppInq', $employee_no] 
+        );
+
+        return response()->json([
+            'success' => true,
+            'data' => $results,
+        ], 200);
+    } catch (\Exception $e) {
+        return response()->json([
+            'success' => false,
+            'message' => $e->getMessage(),
+        ], 500);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+ // ** OB Application Inquiry History
+public function getAppHistory(Request $request) {
+
+    $request->validate([
+        'EMP_NO' => 'required|string',
+        'START_DATE' => 'required|date_format:Y-m-d',
+        'END_DATE' => 'required|date_format:Y-m-d',
+    ]);
+
+    $employee_no = $request->input('EMP_NO');
+    $start_date = $request->input('START_DATE');
+    $end_date = $request->input('END_DATE');
+
+    try {
+        $results = DB::select(
+            'EXEC sproc_PHP_EmpInq_OfficialBusiness @mode = ?, @startdate = ?, @enddate =?, @emp =?',
+            ['AppHistory', $start_date, $end_date, $employee_no] 
+        );
+
+        return response()->json([
+            'success' => true,
+            'data' => $results,
+        ], 200);
+    } catch (\Exception $e) {
+        return response()->json([
+            'success' => false,
+            'message' => $e->getMessage(),
+        ], 500);
+    }
+
+
+}
+
+
+
+
+public function upsert(Request $request)
+{
+    try {
+        $request->validate([
+            'json_data' => 'required|json',
+        ]);
+
+        $params = $request->get('json_data');
+
+      
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid JSON data provided.',
+            ], 400);
+        }
+
+
+        DB::statement('EXEC sproc_PHP_EmpInq_OfficialBusiness @params = :json_data, @mode = :mode', [
+            'json_data' => $params,
+            'mode' => 'upsert'
+        ]);
+
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Transaction saved successfully.',
+        ], 200);
+    } catch (\Exception $e) {
+        Log::error('Transaction save failed:', ['error' => $e->getMessage()]);
+
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Failed to save transaction: ' . $e->getMessage(),
+        ], 500);
+    }
+}
+
+
+};
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
-
+use App\Http\Controllers\LeaveController;
+use App\Http\Controllers\OvertimeController;
+use App\Http\Controllers\OfficialBusinessController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/getLVApprInq', [LeaveController::class, 'getApprInq']);
+Route::post('/getLVApprHistory', [LeaveController::class, 'getApprHistory']);
+Route::post('/getLVAppInq', [LeaveController::class, 'getAppInq']);
+Route::post('/getLVAppHistory', [LeaveController::class, 'getAppHistory']);
+Route::post('/upsertLV', [LeaveController::class, 'upsert']);
+
+
+Route::post('/getOTApprInq', [OvertimeController::class, 'getApprInq']);
+Route::post('/getOTApprHistory', [OvertimeController::class, 'getApprHistory']);
+Route::post('/getOTAppInq', [OvertimeController::class, 'getAppInq']);
+Route::post('/getOTAppHistory', [OvertimeController::class, 'getAppHistory']);
+Route::post('/upsertOT', [OvertimeController::class, 'upsert']);
+
+
+Route::post('/getOBApprInq', [OfficialBusinessController::class, 'getApprInq']);
+Route::post('/getOBApprHistory', [OfficialBusinessController::class, 'getApprHistory']);
+Route::post('/getOBAppInq', [OfficialBusinessController::class, 'getAppInq']);
+Route::post('/getOBAppHistory', [OfficialBusinessController::class, 'getAppHistory']);
+Route::post('/upsertOB', [OfficialBusinessController::class, 'upsert']);
+
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
